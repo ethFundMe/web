2ecf30f1296c0c87f9f47b0945834a9a05b86676
William M. Tsikata <tsikatawill@gmail.com>
2023-11-09 09:24:12 +0000

    fix: build errors

--- src/app/campaigns/create/page.tsx
+++ src/app/campaigns/create/page.tsx
@@ -4,10 +4,10 @@ import { TextSizeStyles } from '@/lib/styles';
 
 export default function CreateCampaignPage() {
   return (
-    <Container className='py-5'>
-      <div className='text-center'>
+    <Container>
+      <div className='py-5 text-center'>
         <h1 className={TextSizeStyles.h1}>Create campaign</h1>
-        <p className='text-lg font-medium'>
+        <p className='font-edium text-lg'>
           Get started by filling out the form below
         </p>
       </div>
