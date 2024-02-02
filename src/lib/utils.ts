500864300b99b2523ce03e24ca05d61710f01d2f
William M. Tsikata <tsikatawill@gmail.com>
2024-02-02 18:23:02 +0000

    chore: use upload preset from env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,7 +38,10 @@ export async function uploadToCloudinary(files: FileList | string[]) {
     const image = files[i];
 
     formData.append('file', image);
-    formData.append('upload_preset', 'neqfirer');
+    formData.append(
+      'upload_preset',
+      process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string
+    );
 
     const resolve = await fetch(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
