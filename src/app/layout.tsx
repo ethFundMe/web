f72945d5f02c422706b63e51431c6da3ef4d0e57
franz@mintgolddust.io <pistisray@gmail.com>
2024-02-13 11:11:07 +0000

    fix: seo Image Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,16 @@ export const metadata: Metadata = {
       'Welcome to EthFundMe - where passion meets potential! Empower your dreams with the ultimate ethereum-powered crowdfunding experience. Harness the blockchain revolution to fuel your projects, ideas, and causes. Join a community driven by innovation, transparency, and the limitless possibilities of decentralized support. Start your journey today with EthFundMe, where every donation counts, and every dream matters. Elevate, innovate, and fundraise with crypto - the future is in your hands!',
     site: '@ethfundme',
     creator: '@ethfundme',
-    images:
-      'https://images.pexels.com/photos/5486872/pexels-photo-5486872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    images: [
+      {
+        url: seoBannerURL(
+          'test_Name',
+          '0.35',
+          'test_title',
+          'test_description'
+        ),
+      },
+    ],
   },
   openGraph: {
     type: 'website',
