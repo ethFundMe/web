d813987293af68c11433bc897c6ef6b000219557
franz@mintgolddust.io <pistisray@gmail.com>
2024-02-19 12:17:51 +0000

    fix: dynamic profile seo setup

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -4,6 +4,36 @@ import { UserProfile } from '@/components/dashboard/UserProfile';
 import { REGEX_CODES } from '@/lib/constants';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata(
+  { params }: Props
+  // parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.slug;
+
+  const user = await getUser(id as `0x${string}`);
+
+  return {
+    title: `${user.fullName}`,
+    description: `${user.bio}`,
+    keywords:
+      'Crypto fundraising, ethFundMe, Eth fundraising, Ethereum fundraising, Blockchain-powered crowdfunding, Decentralized support, Innovation and transparency, Empower your dreams, Community-driven fundraising, Limitless possibilities, Donate with crypto, Donate with eth, Donate with ethereum, Future of fundraising, Blockchain innovation, Cryptocurrency donations',
+    openGraph: {
+      type: 'website',
+      title: `${user.fullName}`,
+      description: `${user.bio}`,
+      url: process.env.NEXT_PUBLIC_WEB_URL,
+    },
+    twitter: {
+      title: `${user.fullName}`,
+      card: 'summary_large_image',
+      description: `${user.bio}`,
+      site: '@ethfundme',
+      creator: '@ethfundme',
+    },
+  };
+}
+
 export default async function UserProfilePage({
   params: { slug },
 }: {
