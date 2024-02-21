f7ed5fb7ce3ebf7895f7678477a3f79e1fe05428
William M. Tsikata <tsikatawill@gmail.com>
2024-02-21 12:34:51 +0000

    fix(seo): move seo to layout

--- src/app/campaigns/[slug]/page.tsx
+++ src/app/campaigns/[slug]/page.tsx
@@ -8,81 +8,10 @@ import { TextSizeStyles } from '@/lib/styles';
 import { cn, formatWalletAddress } from '@/lib/utils';
 import dayjs from 'dayjs';
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
 import { DonationObjectiveIndicator } from '../DonationObjectiveIndicator';
 
 import ImageWithFallback from '@/components/ImageWithFallback';
 import { SwiperCarousel } from '@/components/SwiperCarousel';
-import { seoCampaign } from '@/lib/seoBannerUrl';
-import type { Metadata, ResolvingMetadata } from 'next';
-import { formatEther } from 'viem';
-
-type Props = {
-  params: { slug: string };
-};
-
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const id = params.slug;
-
-  const campaign = await getCampaign(parseInt(id));
-  if (!campaign) notFound();
-
-  const user = await getUser(campaign.creator as `0x${string}`);
-  if (!user) notFound();
-
-  const previousMetaData = await parent;
-
-  return user
-    ? {
-        title: `${campaign.title}`,
-        description: `${campaign.description}`,
-        keywords:
-          'Crypto fundraising, ethFundMe, Eth fundraising, Ethereum fundraising, Blockchain-powered crowdfunding, Decentralized support, Innovation and transparency, Empower your dreams, Community-driven fundraising, Limitless possibilities, Donate with crypto, Donate with eth, Donate with ethereum, Future of fundraising, Blockchain innovation, Cryptocurrency donations',
-        openGraph: {
-          type: 'website',
-          title: `${campaign.title}`,
-          description: `${campaign.description}`,
-          images: [
-            {
-              url: seoCampaign(
-                user.fullName,
-                parseFloat(formatEther(BigInt(campaign.goal))),
-                campaign.title,
-                campaign.description,
-                campaign?.media_links[0] || ''
-              ),
-            },
-          ],
-          url: 'https://ethfund.me',
-        },
-        twitter: {
-          title: `${campaign.title}`,
-          card: 'summary_large_image',
-          description: `${campaign.description}`,
-          images: [
-            {
-              url: seoCampaign(
-                user.fullName,
-                parseFloat(formatEther(BigInt(campaign.goal))),
-                campaign.title,
-                campaign.description,
-                campaign?.media_links[0] || ''
-              ),
-            },
-          ],
-          site: '@ethfundme',
-          creator: '@ethfundme',
-        },
-      }
-    : {
-        title: previousMetaData.title,
-        description: previousMetaData.description,
-      };
-}
 
 export default async function CampaignPage({
   params: { slug },
