f20fe2a4d7f586159ceca9817a8369932223db3b
William M. Tsikata <tsikatawill@gmail.com>
2024-05-18 11:15:03 +0000

    feat: add 404 broken image

--- src/components/DashboardLoadingSpinner.tsx
+++ src/components/DashboardLoadingSpinner.tsx
@@ -1,20 +1,16 @@
-import { Loader } from 'lucide-react';
 import Image from 'next/image';
 
 export default function DashboardLoadingSpinner() {
   return (
-    <div className='mx-auto mt-20 flex h-[50vh] max-h-[600px] max-w-sm flex-col'>
+    <div className='mx-auto mt-20 flex h-[50vh] max-h-[600px] max-w-sm animate-pulse flex-col'>
       <Image
         className='mx-auto opacity-50'
-        src='/images/efm-logo.svg'
-        width={200}
-        height={200}
+        src='/images/Logo-Virgin.png'
+        width={150}
+        height={150}
         alt='logo'
+        priority
       />
-
-      <div className='mx-auto mt-8 w-fit'>
-        <Loader className='animate-spin text-primary-default' />
-      </div>
     </div>
   );
 }
