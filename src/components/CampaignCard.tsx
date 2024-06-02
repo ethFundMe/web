1bad50134e33db07a4d7af8b8d2a566370d9e341
franz@mintgolddust.io <pistisray@gmail.com>
2024-06-02 19:56:25 +0000

    fix: campaign carousel fix

--- src/components/CampaignCard.tsx
+++ src/components/CampaignCard.tsx
@@ -25,93 +25,100 @@ export const CampaignCard = ({
   const user = campaign.user;
 
   return (
-    <Link
-      href={`/campaigns/${campaign.campaign_id}`}
-      // onClick={() => router.push(`/campaigns/${campaign.campaign_id}`)}
-      className={cn(
-        'group flex cursor-pointer flex-col gap-1 rounded-md border border-primary-gray bg-white p-4 hover:border-primary-default',
-        !full && 'w-full flex-shrink-0 md:max-w-[400px]',
-        variantStyles,
-        campaign.flagged && 'border-red-500 hover:border-red-500'
-      )}
+    <div
+      className={`border pb-4 ${
+        campaign.flagged
+          ? 'border-red-500 hover:border-red-500'
+          : 'rounded-md border-primary-gray hover:border-primary-default'
+      }`}
     >
-      <div
+      <Link
+        href={`/campaigns/${campaign.campaign_id}`}
+        // onClick={() => router.push(`/campaigns/${campaign.campaign_id}`)}
         className={cn(
-          'h-80 overflow-hidden bg-slate-200 md:h-48 lg:h-60',
-          campaign.flagged && 'grayscale'
+          'group flex cursor-pointer flex-col gap-1  bg-white p-4',
+          !full && 'w-full flex-shrink-0 md:max-w-[400px]',
+          variantStyles
         )}
       >
-        <ImageWithFallback
-          className='h-full w-full object-cover transition-all duration-300 ease-in group-hover:scale-[1.03]'
-          src={campaign.banner_url ?? '/images/broken.jpg'}
-          height={240}
-          width={300}
-          alt='...'
-        />
-      </div>
-
-      <DonationObjectiveIndicator
-        className={cn(campaign.flagged && 'grayscale')}
-        currentAmount={campaign.total_accrued}
-        seekingAmount={campaign.goal}
-      />
-
-      <div className='flex-1'>
-        <p className='line-clamp-1 text-xl font-semibold'>{campaign.title}</p>
-        <p className='line-clamp-2 text-neutral-700 md:min-h-12'>
-          {campaign.description}
-        </p>
-      </div>
-
-      <div className='space-y-2'>
-        {campaign.creator !== campaign.beneficiary && (
-          <p
-            className={cn(TextSizeStyles.small, 'text-primary-default')}
-            onClick={(e) => e.preventDefault()}
-          >
-            Organized for{' '}
-            <span className={cn(TextSizeStyles.caption, 'font-semibold')}>
-              {formatWalletAddress(campaign.beneficiary, 'short')}
-            </span>
-          </p>
-        )}
-
-        <Link
-          href={`/profile/${campaign.creator}`}
-          onClick={(e) => {
-            e.stopPropagation();
-            // router.push(`/profile/${campaign.creator}`);
-          }}
+        <div
           className={cn(
-            'flex w-full cursor-pointer items-center gap-4 rounded-md bg-slate-100 p-3 hover:bg-slate-200',
+            'h-80 overflow-hidden bg-slate-200 md:h-48 lg:h-60',
             campaign.flagged && 'grayscale'
           )}
         >
-          <div className='relative h-[48px] w-[48px] flex-shrink-0'>
-            <ImageWithFallback
-              src={(user && user.profileUrl) ?? ''}
-              fallback='/images/user-pfp.png'
-              className='rounded-full bg-slate-200 object-cover'
-              fill
-              sizes='48px'
-              alt='...'
-            />
-          </div>
+          <ImageWithFallback
+            className='h-full w-full object-cover transition-all duration-300 ease-in group-hover:scale-[1.03]'
+            src={campaign.banner_url ?? '/images/broken.jpg'}
+            height={240}
+            width={300}
+            alt='...'
+          />
+        </div>
 
-          <div>
-            <p className={TextSizeStyles.small}>Organizer</p>
+        <DonationObjectiveIndicator
+          className={cn(campaign.flagged && 'grayscale')}
+          currentAmount={campaign.total_accrued}
+          seekingAmount={campaign.goal}
+        />
+
+        <div className='flex-1'>
+          <p className='line-clamp-1 text-xl font-semibold'>{campaign.title}</p>
+          <p className='line-clamp-2 text-neutral-700 md:min-h-12'>
+            {campaign.description}
+          </p>
+        </div>
+
+        <div className='space-y-2'>
+          {campaign.creator !== campaign.beneficiary && (
             <p
-              className={cn(
-                TextSizeStyles.caption,
-                'line-clamp-1 w-full font-semibold [word-break:break-all]'
-              )}
+              className={cn(TextSizeStyles.small, 'text-primary-default')}
+              onClick={(e) => e.preventDefault()}
             >
-              {(user && user.fullName) ??
-                formatWalletAddress(campaign.creator as `0x${string}`)}
+              Organized for{' '}
+              <span className={cn(TextSizeStyles.caption, 'font-semibold')}>
+                {formatWalletAddress(campaign.beneficiary, 'short')}
+              </span>
             </p>
-          </div>
-        </Link>
-      </div>
-    </Link>
+          )}
+
+          <Link
+            href={`/profile/${campaign.creator}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              // router.push(`/profile/${campaign.creator}`);
+            }}
+            className={cn(
+              'flex w-full cursor-pointer items-center gap-4 rounded-md bg-slate-100 p-3 hover:bg-slate-200',
+              campaign.flagged && 'grayscale'
+            )}
+          >
+            <div className='relative h-[48px] w-[48px] flex-shrink-0'>
+              <ImageWithFallback
+                src={(user && user.profileUrl) ?? ''}
+                fallback='/images/user-pfp.png'
+                className='rounded-full bg-slate-200 object-cover'
+                fill
+                sizes='48px'
+                alt='...'
+              />
+            </div>
+
+            <div>
+              <p className={TextSizeStyles.small}>Organizer</p>
+              <p
+                className={cn(
+                  TextSizeStyles.caption,
+                  'line-clamp-1 w-full font-semibold [word-break:break-all]'
+                )}
+              >
+                {(user && user.fullName) ??
+                  formatWalletAddress(campaign.creator as `0x${string}`)}
+              </p>
+            </div>
+          </Link>
+        </div>
+      </Link>
+    </div>
   );
 };
