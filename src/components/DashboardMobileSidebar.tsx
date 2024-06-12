4ccccd5942a463f80a324423ce11f5bbbc877a58
William M. Tsikata <tsikatawill@gmail.com>
2024-06-12 05:45:59 +0000

    fix: earnings page crashing on mobile

--- src/components/DashboardMobileSidebar.tsx
+++ src/components/DashboardMobileSidebar.tsx
@@ -8,8 +8,8 @@ export default function DashboardMobileSidebar({
   routes: { link: string; title: string; icon: React.ReactNode }[];
 }) {
   return (
-    <div className='fixed bottom-6 left-1/2 z-20 mx-auto block w-full  max-w-[80%] -translate-x-1/2 rounded-3xl border border-slate-200 bg-white opacity-100 hover:border-slate-200 hover:opacity-100 sm:border-transparent sm:opacity-80 md:hidden'>
-      <ul className='flex items-center justify-around gap-5 px-4 py-2'>
+    <div className='fixed bottom-6 left-1/2 z-20 mx-auto block w-full max-w-[95%] -translate-x-1/2 rounded-3xl border border-slate-200 bg-white opacity-100 hover:border-slate-200 hover:opacity-100 sm:max-w-[80%] sm:border-transparent sm:opacity-80 md:hidden'>
+      <ul className='flex items-center justify-around gap-1 px-1 py-2 sm:gap-5 sm:px-4'>
         {routes.map((route, idx) => (
           <li key={idx} title={route.title}>
             <NavLink
