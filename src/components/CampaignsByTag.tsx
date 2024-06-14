a942e7c27c5df8057ebc38a897614a6652228dcb
franz@mintgolddust.io <pistisray@gmail.com>
2024-06-14 17:14:17 +0000

    feat: useswr switch

--- src/components/CampaignsByTag.tsx
+++ src/components/CampaignsByTag.tsx
@@ -1,13 +1,20 @@
-import { getCampaigns } from '@/lib/queries';
+// 'use client';
+// import { getCampaigns } from '@/lib/queries';
 import 'react-multi-carousel/lib/styles.css';
 import CampaignsCarousel from './CampaignsCarousel';
+// import useSWR from 'swr';
 
-export default async function CampaignsByTag({ tag }: { tag: string }) {
-  const { campaigns } = await getCampaigns({ page: 1, tag });
+// const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-  return (
-    campaigns.length > 0 && (
-      <CampaignsCarousel campaigns={campaigns} tag={tag} />
-    )
-  );
+export default function CampaignsByTag({ tag }: { tag: string }) {
+  // const { campaigns } = await getCampaigns({ page: 1, tag });
+  // const apiBaseUrl = process.env.NEXT_PUBLIC_ETH_FUND_ENDPOINT || '';
+  // const { data: campaigns, isLoading } = useSWR(
+  //   `${apiBaseUrl}/api/notifications?tags=${tag}`,
+  //   fetcher
+  // );
+
+  // if(isLoading) return <div>loading...</div>
+
+  return <CampaignsCarousel tag={tag} />;
 }
--- src/components/CampaignsCarousel.tsx
+++ src/components/CampaignsCarousel.tsx
@@ -1,19 +1,30 @@
 'use client';
-import { Campaign } from '@/types';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import useSWR from 'swr';
 import { CampaignCard } from './CampaignCard';
 
-const CampaignsCarousel = ({
-  campaigns,
-  tag,
-}: {
-  campaigns: Campaign[];
-  tag: string;
-}) => {
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+interface Meta {
+  limit: number;
+  page: number;
+  totalCampaigns: number;
+  totalPages: number;
+}
+
+const CampaignsCarousel = ({ tag }: { tag: string }) => {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_ETH_FUND_ENDPOINT || '';
+  const { data, isLoading } = useSWR<{ campaigns: Camapaign[]; meta: Meta }>(
+    `${apiBaseUrl}/api/campaigns?tag=${tag}`,
+    fetcher
+  );
+  if (isLoading) return <div>loading...</div>;
+
+  const { campaigns } = data;
+  console.log(campaigns);
   return (
     <div className='w-full space-y-14 md:pl-6'>
       <div className='space-y-5'>
@@ -31,7 +42,7 @@ const CampaignsCarousel = ({
           spaceBetween='40px'
           modules={[Pagination, Navigation]}
         >
-          {campaigns.map((item) => {
+          {campaigns?.map((item) => {
             return (
               <SwiperSlide key={item.id} className='md:max-w-[400px]'>
                 {' '}
