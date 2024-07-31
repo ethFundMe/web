2796771e97098c2773907d5371509e664423aa4c
bsaii <bernardsaii20@gmail.com>
2024-07-31 10:52:12 +0000

    feat: livepeer video player

--- src/components/SwiperCarousel.tsx
+++ src/components/SwiperCarousel.tsx
@@ -1,6 +1,16 @@
 'use client';
 
 import { REGEX_CODES } from '@/lib/constants';
+import { PauseIcon, PlayIcon } from '@livepeer/react/assets';
+import { getSrc, LivepeerPlaybackInfo } from '@livepeer/react/external';
+import {
+  Container,
+  Controls,
+  PlayingIndicator,
+  PlayPauseTrigger,
+  Root,
+  Video,
+} from '@livepeer/react/player';
 import YouTubePlayer from 'react-player/youtube';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,7 +19,13 @@ import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import ImageWithFallback from './ImageWithFallback';
 
-export const SwiperCarousel = ({ images }: { images: string[] }) => {
+export const SwiperCarousel = ({
+  images,
+  playbackInfo,
+}: {
+  images: string[];
+  playbackInfo: LivepeerPlaybackInfo | undefined;
+}) => {
   // const AnimImage = motion(Image);
 
   return (
@@ -32,6 +48,8 @@ export const SwiperCarousel = ({ images }: { images: string[] }) => {
               <>
                 <YouTubePlayer url={mediaLink} width='100%' height='100%' />
               </>
+            ) : REGEX_CODES.livepeerId.test(mediaLink) ? (
+              <LivepeerPlayer info={playbackInfo} />
             ) : (
               <ImageWithFallback
                 className='h-full flex-shrink-0 object-contain'
@@ -47,3 +65,25 @@ export const SwiperCarousel = ({ images }: { images: string[] }) => {
     </div>
   );
 };
+
+function LivepeerPlayer({ info }: { info: LivepeerPlaybackInfo | undefined }) {
+  const src = getSrc(info);
+  return (
+    <Root src={src}>
+      <Container className='h-full w-full overflow-hidden bg-gray-950'>
+        <Video title='Live stream' className='h-full w-full' />
+
+        <Controls className='flex items-center justify-center'>
+          <PlayPauseTrigger className='h-10 w-10 flex-shrink-0 hover:scale-105'>
+            <PlayingIndicator asChild matcher={false}>
+              <PlayIcon className='h-full w-full' />
+            </PlayingIndicator>
+            <PlayingIndicator asChild>
+              <PauseIcon className='h-full w-full' />
+            </PlayingIndicator>
+          </PlayPauseTrigger>
+        </Controls>
+      </Container>
+    </Root>
+  );
+}
